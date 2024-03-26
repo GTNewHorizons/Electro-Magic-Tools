4ded9a0a2c96ee56569597cc572cf27ff8873f46
glowredman <35727266+glowredman@users.noreply.github.com>
2024-03-26 14:47:57 +0100

    Deobfuscate parameters

--- src/main/java/emt/command/CommandOutputs.java
+++ src/main/java/emt/command/CommandOutputs.java
@@ -97,7 +97,7 @@ public class CommandOutputs extends CommandBase {
     }
 
     @Override
-    public String getCommandUsage(ICommandSender p_71518_1_) {
+    public String getCommandUsage(ICommandSender sender) {
         return "/emt_outputs";
     }
 
