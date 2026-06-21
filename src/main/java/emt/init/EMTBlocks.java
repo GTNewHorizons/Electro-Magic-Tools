49554c94bb3408289da8fa402e602b843030875c
LazyFlesh <40587985+LazyFlesh@users.noreply.github.com>
2026-06-21 14:31:38 -0700

    Deprecate ic2 iron fence (#119)

--- src/main/java/emt/init/EMTBlocks.java
+++ src/main/java/emt/init/EMTBlocks.java
@@ -14,6 +14,7 @@ import emt.item.block.ItemBlockElectricCloud;
 import emt.item.block.ItemBlockEssentiaGenerators;
 import emt.item.block.ItemBlockMachines;
 import emt.tile.solar.Solars;
+import gregtech.api.enums.ItemList;
 import ic2.api.item.IC2Items;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
@@ -49,7 +50,7 @@ public class EMTBlocks {
     public static void addAspects() {
         registerObjectTag(IC2Items.getItem("rubberTrampoline"), new AspectList().add(Aspect.AIR, 5));
         registerObjectTag(
-                IC2Items.getItem("ironFence"),
+                ItemList.FenceIron.get(1L),
                 new AspectList().add(Aspect.ENERGY, 1).add(Aspect.METAL, 2).add(Aspect.WEATHER, 1));
         registerObjectTag(
                 IC2Items.getItem("reinforcedGlass"),
